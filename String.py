cde0f887560a75f6ba97ea034b94229a80816986
Kadiyala-Ramya <122162589+Kadiyala-Ramya@users.noreply.github.com>
2025-03-24 18:47:55 +0530

    Add files via upload

---
+++ String.py
@@ -0,0 +1,6 @@
+#Task:1 Practice all Methods
+"coders".upper()
+"CODERS".lower()
+"I like guava".replace("guava","apple")
+"we are devlopers".split(",")
+#Task2 Take a string it contains both upper and lower case ,print the no of vowels and consonants present in the string

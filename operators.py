6bdbca303ecb5afadb04b6bd6f726193147753a4
Kadiyala-Ramya <122162589+Kadiyala-Ramya@users.noreply.github.com>
2025-03-28 21:27:27 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,49 @@
+x=10
+y=5
+print("Arithmetic operators")
+print("Addition",x+y)
+print("Subtraction",x-y)
+print("Multiplication",x*y)
+print("Division",x%y)
+print("Modular division",x+y)
+print("Assignment operators")
+x+=3
+print("+=",x)
+x-=3
+print("-=",x)
+x*=3
+print("*=",x)
+x/=3
+print("/=",x)
+x%=3
+print("%=",x)
+print("Logical operators")
+print("AND",x>5 and x<15)
+print("OR",x>5 or x<4)
+print("NOT",not(x>11 and x<5))
+print("Bitwise operators")
+x=10
+y=5
+print("&",x&y)
+print("|",x|y)
+print("^",x^y)
+print("<<",x<<2)
+print(">>",x>>2)
+x=10
+y=5
+print("Comparison operators")
+print("==",x==y)
+print("!=",x!=y)
+print(">",x>y)
+print("<",x<y)
+print(">=",x>=y)
+print("<=",x<=y)
+print("Identity operators")
+x=5
+y=6
+print("is",x is y)
+print("is not",x is not y)
+print("Membership operators")
+x=[1,2,3]
+print("in",1 in x)
+print("not in",2 not in x)
---
+++ operators1.py
@@ -0,0 +1,28 @@
+#1.Write a function that takes two numbers as input and performs addition.
+print("function that takes two numbers as input and performs addition")
+def add_numbers(num1, num2):
+    return num1 + num2
+result = add_numbers(10, 5)
+print("Sum of two numbers:", result)  
+#2.Create a function that accepts a name and displays a personalized greeting.
+print("function that accepts a name and displays a personalized greeting")
+def user(name):
+    print("Hello",name,"Welcome")
+user("Ramya!")
+#3.Write a function to calculate the square of a given number and show the result.
+print("function to calculate the square of a given number and show the result")
+def sqr_numbers(x):
+    return x**2
+result=sqr_numbers(5)
+print("Square of number",result)
+#4.Write a function to multiply two numbers and display the result.
+print("function to multiply two numbers and display the result")
+def mul_numbers(x,y):
+    return x*y
+result = mul_numbers(10,5)
+print("Multiplication of two numbers",result)
+#5.Create a function that takes a string as input and prints the length of the string.(need to do research and implement)
+print("function that takes a string as input and prints the length of the string")
+def length(str):
+    print("length of string",len(str))
+length("Rama kills ravana")

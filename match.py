e074704a3762b893cb999d8def74c6be408b0d34
Kadiyala-Ramya <122162589+Kadiyala-Ramya@users.noreply.github.com>
2025-03-28 22:12:00 +0530

    Add files via upload

---
+++ match.py
@@ -0,0 +1,29 @@
+#write a program using "Match" conditional statement which takes input from 1 to 12 and prints month name for each case
+x= int(input("Enter your number:"))
+match x:
+    case 1:
+        print("January")
+    case 2:
+        print("February")
+    case 3:
+        print("March")
+    case 4:
+        print("April")
+    case 5:
+        print("May")
+    case 6:
+        print("June")
+    case 7:
+        print("July")
+    case 8:
+        print("August")
+    case 9:
+        print("September")
+    case 10:
+        print("October")
+    case 11:
+        print("November")
+    case 12:
+        print("December")
+    case _:
+        print("Invalid")
